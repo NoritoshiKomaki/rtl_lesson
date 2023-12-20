3451ca3c6c8d92116677de288b7f41fabce55570
NoritoshiKomaki <97800895+NoritoshiKomaki@users.noreply.github.com>
2023-12-21 00:15:34 +0900

    kokokoo

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-const axios = require('axios'); 
+const { IncomingWebhook } = require("@slack/webhook");
 
-
-
-    const data = {
-        "text": "料理が新しく投稿されました!"
-    }
-
-    axios.post(
-        {"https://hooks.slack.com/services/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"},
-        JSON.stringify(data),
-        // corsエラー回避
-        {
-            withCredentials: false,
-            transformRequest: [(data, headers) => {
-            delete headers.post["Content-Type"]
-            return data
-            }]
-        }
-    )
-    .then(res => {
-      console.log(res)
-    })
-    .catch(e => {
-      console.log(e)
-    });
+const webhook = new IncomingWebhook(
+  "https://hooks.slack.com/services/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"
+)(async () => {
+  await webhook.send({
+    text: "hello",
+  });
+})();
