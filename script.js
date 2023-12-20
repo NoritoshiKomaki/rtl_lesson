d80be2b03ca5101a31be3e3a15d6f75e1df60b2e
NoritoshiKomaki <97800895+NoritoshiKomaki@users.noreply.github.com>
2023-12-21 00:23:58 +0900

    ji

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const { IncomingWebhook } = require("@slack/webhook");
 
 const webhook = new IncomingWebhook(
   "https://hooks.slack.com/services/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"
-)(async () => {
+);
+
+(async () => {
   await webhook.send({
     text: "hello",
   });
